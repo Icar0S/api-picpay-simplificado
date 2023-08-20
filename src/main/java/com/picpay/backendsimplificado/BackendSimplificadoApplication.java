5b58d12ac07637b0a51e62474b438862ae7e9721
icar0s <icarooliveira_23@hotmail.com>
2023-08-19 22:33:48 -0300

    feat: create configurations

---
+++ src/main/java/com/picpay/backendsimplificado/BackendSimplificadoApplication.java
@@ -0,0 +1,13 @@
+package com.picpay.backendsimplificado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSimplificadoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendSimplificadoApplication.class, args);
+    }
+
+}
